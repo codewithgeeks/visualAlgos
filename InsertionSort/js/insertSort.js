3c0b3888e46e469ac3868167e39fecfe1d12f2da
piyushlahoti <plwhizkid@gmail.com>
2014-12-09 14:56:23 +0530

    Basic Working skeleton

---
+++ InsertionSort/js/insertSort.js
@@ -0,0 +1,15 @@
+/*Javascript file for Insertion Sort Algorithm page*/
+var curModule = angular.module('insertSort',['ngRoute']);
+
+curModule.controller("animator",
+	function($scope)
+	{
+		$scope.integerList = [];
+		$scope.newInt = 1;
+		$scope.addNewInt = 	function(newI)
+							{
+								$scope.integerList.push(newI);
+								$scope.integerList.sort(function(a, b){return a-b});
+							}
+	}
+);
